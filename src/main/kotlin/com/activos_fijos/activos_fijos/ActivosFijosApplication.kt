5638a4b5e9d8f32c155340dc72e0b997bf28c7f6
jmamian <julian079620@gmail.com>
2020-12-06 18:53:19 -0500

    primer commit

---
+++ src/main/kotlin/com/activos_fijos/activos_fijos/ActivosFijosApplication.kt
@@ -0,0 +1,11 @@
+package com.activos_fijos.activos_fijos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActivosFijosApplication
+
+fun main(args: Array<String>) {
+	runApplication<ActivosFijosApplication>(*args)
+}
---
+++ src/test/kotlin/com/activos_fijos/activos_fijos/ActivosFijosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.activos_fijos.activos_fijos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActivosFijosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
